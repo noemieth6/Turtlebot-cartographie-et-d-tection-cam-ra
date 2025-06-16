4176632217655e81d396c33944d54fb523b56749
Agathe Rabasse <agathe.rabasse@etu.utc.fr>
2025-06-16 21:13:18 +0000

    ajout commentaires

--- src/lidar/lidar/clusterer.py
+++ src/lidar/lidar/clusterer.py
@@ -4,18 +4,28 @@ import numpy as np
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs_py.point_cloud2 import create_cloud, read_points_numpy
+from sensor_msgs.msg import PointField
 
 
 class Clusterer(Node):
     def __init__(self):
         super().__init__("clusterer")
         
+        #---Publisher---#
         self.pub = self.create_publisher(PointCloud2, "clusters", 10)
+
+        #---Subscriber---#
         self.sub = self.create_subscription(PointCloud2, "points", self.callback, 10)
+        
+        #---Initialisation des points cumulés---#
         self.allpoints = []
         
+        self.get_logger().info("Clusterer node started")
+        
+
 
     def callback(self, msg: PointCloud2):
+  
         xy = read_points_numpy(msg, ["x", "y"])
 
         # Apply your custom cluster function
@@ -31,17 +41,20 @@ class Clusterer(Node):
         cluster_ids, cluster_counts = np.unique(clusters, return_counts=True)
         counts = cluster_counts[cluster_ids[clusters]]
 
-        # TODO: Filter points based on cluster ID and size
+        # Tri des points
         filtered_points = []
         for i in range(len(clustered_points)):
             if (counts[i] > 5) and (clustered_points[i, 4] !=0):
                 filtered_points.append(clustered_points[i])                    
 
-        self.allpoints.append(filtered_points)       
-        print("arrivé jusque là")
-        print("self.allpoints[0]:", self.allpoints[0])
+        # Ajout à une liste globale pour affichage de la structure du labyrinthe
+        self.allpoints.append(filtered_points)      
+
+        # Forcer le frame_id
         msg.header.frame_id = "odom" 
-        self.pub.publish(create_cloud(msg.header, msg.fields, np.vstack(self.allpoints))) # renvoie la dernière création du nuage de points complet
+
+        # Renvoie la dernière création du nuage de points complet
+        self.pub.publish(create_cloud(msg.header, msg.fields, np.vstack(self.allpoints))) 
  
 
     def cluster(self, points: np.ndarray) -> np.ndarray:
@@ -51,12 +64,12 @@ class Clusterer(Node):
         :return: Array of [c0, c1, ...]
         """
         C = np.zeros(points.shape[0], dtype=int)
-        # TODO: Determine parameter values
+        # Parameter values
         k = 3
         D = 0.1
        
 
-        # TODO: Implement the clustering algorithm
+        # Clustering algorithm
         for i in range(k,len(points)):
             dist = [0 for _ in range(k-1)]
             for j in range(1,k):
